165a7c8a922ae55fa20cd99e6c570bf0c3b5cbeb
Eglantine <antonscheglow40o@gmail.com>
2025-03-25 17:25:39 +0300

    change size of hash from 512bit to 256

--- lib/Hash293.cpp
+++ lib/Hash293.cpp
@@ -4,7 +4,7 @@
 std::string Hash293::toString(const char* data) {
     std::stringstream result;
 
-    for (int i = 0; i < 64; ++i) {
+    for (int i = 0; i < 32; ++i) {
         result << std::hex << std::setw(2) << std::setfill('0') 
                << static_cast<int>(static_cast<unsigned char>(data[i]));
     }
@@ -15,12 +15,12 @@ std::string Hash293::toString(const char* data) {
 char* Hash293::hash293(const char* data, uint32_t dataSize) {
     int len = dataSize;
 
-    int minSize = 64;
+    int minSize = 32;
     while (minSize < len) {
-        minSize <<= 1;
+        minSize <<= 1; 
     }
 
-    int add = minSize - len; // Number of padding bytes to add
+    int add = minSize - len;
     int block = 0;
     for (int i = 0; i < len; ++i) {
         block += data[i];
@@ -37,7 +37,8 @@ char* Hash293::hash293(const char* data, uint32_t dataSize) {
     delete[] paddedData;
 
     int prevSize = paddedSize;
-    while (prevSize > 64) {
+
+    while (prevSize > 32) {
         int newSize = prevSize / 2;
         int* temp = new int[newSize];
 
@@ -51,18 +52,11 @@ char* Hash293::hash293(const char* data, uint32_t dataSize) {
         prevSize = newSize;
     }
 
-    char* hashArr = new char[prevSize];
-    for (int i = 0; i < prevSize; ++i) {
-        int mixed = whitening((prev[i] * 41) ^ (i * 29) ^ (i << 5), i);
-        hashArr[i] = static_cast<char>(mixed);
+    char* hashArr = new char[32];
+    for (int i = 0; i < 32; ++i) {
+        hashArr[i] = static_cast<char>(whitening(prev[i] ^ prev[(i + 16) % 32], i));
     }
 
     delete[] prev;
-
-    for (int i = 0; i < prevSize; ++i) {
-        int tmp = static_cast<unsigned char>(hashArr[i]);
-        hashArr[i] = static_cast<char>(whitening(tmp + i, i));
-    }
-
     return hashArr;
 }
--- lib/hash_utils.cpp
+++ lib/hash_utils.cpp
@@ -2,8 +2,8 @@
 
 
 char* fill_array(const char* data, int dataSize, int add, int block) {
-    char* newData = new char[dataSize + add]; // Create a new array
-    std::memcpy(newData, data, dataSize); // Copy original data
+    char* newData = new char[dataSize + add];
+    std::memcpy(newData, data, dataSize);
 
     for (int i = 0; i < add; ++i) {
         newData[dataSize + i] = static_cast<char>(whitening(block + i, i));
